dc7e8d62091cd1f7ee4570b99cf588f9aa0b5060
soungupta <soungupta@ebay.com>
2019-01-14 11:19:23 -0800

    made corrections for autotools compilation

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -2,24 +2,10 @@
 #include <isa-l/crc.h>
 
 int main(void) {
-    isal_version_info_data* id = isal_version_info(ISALVERSION_NOW);
-    if (!id) {
-        return 1;
+    if ( crc16_t10dif(0x8005, "32311E333530", 12) != 0xb4c9 ) {
+        std::cout << "Failed to install ISA-L\n";
+        return -1;
     }
-    std::cout << "protocols: ";
-    for(auto proto = id->protocols; *proto; proto++) {
-        std::cout << *proto;
-    }
-    std::cout   << "\nversion: "        << id->version
-                << "\nssl version: "    << id->ssl_version
-                << "\nfeatures: "       << id->features
-                << "\n";
-
-    if ( crc16_t10dif(0x8005, "32311E333530", 12) == 0xb4c9 ) {
-        std::cout << "Succeed\n";
-    } else {
-        std::cout << "Failed to init isal\n";
-        retval = 3;
-    }
-    return retval;
+    std::cout << "Success\n";
+    return 0;
 }
